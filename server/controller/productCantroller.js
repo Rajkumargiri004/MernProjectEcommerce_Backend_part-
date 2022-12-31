e4b6b57da8dae9c518d511e6494c1f4fa90c2415
Rajkumar Giri <88571625+Rajkumargiri004@users.noreply.github.com>
2022-12-31 09:26:07 +0530

    crud in backend
     i  have update get single product , deleteproduct, update product all product

--- server/controller/productCantroller.js
+++ server/controller/productCantroller.js
@@ -10,6 +10,75 @@ exports.createProduct = async (req,res,next)=>{
         productdemo
     })
 }
+// get all product --Admin
 
-// get all product
+exports.getAllproduct = async (req,res)=>{
+    const responceproduct = await allproducts.find()
+    res.status(200).json({
+      success: true,
+      responceproduct
+    })
+    
+}
+
+// update product ---Admin
+
+exports.updateproduct = async(req,res)=>{
+    let product = await allproducts.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "product not found"
+        })
+    }
+    else{
+        product = await allproducts.findByIdAndUpdate(req.params.id,req.body,{
+            new: true,
+            runValidators: false,
+            useFindAndModify: false
+        });
+
+    }
+    res.status(200).json({
+        success: true,
+        product
+
+    })
+}
+// Delete product ---Admin
+
+exports.DeleteProduct = async(req,res)=>{
+    const delproduct = await allproducts.findById(req.params.id)
+
+    if(!delproduct){
+        return res.status(500).json({
+            message: "product not found"
+        })
+    }
+    else{
+        await delproduct.remove()
+        res.status(200).json({
+            message: "product delete sucessfully"
+        })
+    }
+}
+
+// Get single product
+
+exports.getsingleproduct = async(req,res)=>{
+    const singleprod = await allproducts.findById(req.params.id);
+    if(!singleprod){
+       return res.status(500).json({
+        sucess: false,
+        message: "product not found"
+        })
+    }
+    
+    res.status(200).json({
+        success: true,
+        singleprod
+
+    })
+}
+
 
